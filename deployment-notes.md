d379848eeb7794b9f83be331e6b4afa54f95a95f
Pim Kunis <pim@kunis.nl>
2019-06-06 20:40:36 +0200

    add auth controller tests

--- deployment-notes.md
+++ deployment-notes.md
@@ -1,5 +1,4 @@
 ### Updating on the deploy server
-mix edeliver update production --branch=master --start-deploy
---skip-git-clean
+mix edeliver update production --branch=master --start-deploy --skip-git-clean
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 4000
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4001
--- test/personal_website_web/controllers/auth_controller_test.exs
+++ test/personal_website_web/controllers/auth_controller_test.exs
@@ -1,22 +1,49 @@
 defmodule PersonalWebsiteWeb.AuthControllerTest do
   use PersonalWebsiteWeb.ConnCase
 
-  test "GET /auth", %{conn: conn} do
-    conn = get(conn, "/auth")
-    assert html_response(conn, 200) =~ "Authentication"
+  describe "when a user visits the auth page" do
+    test "the page loads", %{conn: conn} do
+      conn = get(conn, "/auth")
+      assert html_response(conn, 200) =~ "Authentication"
+    end
+
+    test "they are not logged in as admin", %{conn: conn} do
+      conn = get(conn, "/auth")
+      refute html_response(conn, 200) =~ "Logout"
+    end
   end
 
-  test "Admin login/logout", %{conn: conn} do
-    conn = get(conn, "/auth")
-    refute html_response(conn, 200) =~ "Logout"
+  describe "when a user logs in as admin" do
+    setup [:log_in]
 
-    password = "open sesam"
+    test "an error is reported if the password is wrong", %{conn: conn} do
+      assert html_response(conn, 200) =~ "incorrect"
+    end
+
+    test "they can successfully log in with the correct password", %{conn: conn} do
+      refute html_response(conn, 302) =~ "incorrect"
+    end
+
+    test "they can successfully log out again", %{conn: conn} do
+      delete(conn, "/auth")
+      assert conn.status == 302
+
+      conn = get(conn, "/auth")
+      assert html_response(conn, 200) =~ "Authentication"
+    end
+  end
+
+  defp log_in(context) do
+    password = "sesam"
     System.put_env("ADMIN_PASS_HASH", Argon2.hash_pwd_salt(password))
 
-    conn = post(conn, "/auth", password: "Wrong!")
-    assert html_response(conn, 200) =~ "incorrect"
+    conn = cond do
+      to_string(context.test) =~ "wrong" ->
+        post(context.conn, "/auth", password: "Wrong!")
+      true ->
+        post(context.conn, "/auth", password: password)
+    end
 
-    conn = post(conn, "/auth", password: password)
-    refute html_response(conn, 302) =~ "incorrect"
+    %{context | conn: conn}
   end
 end
