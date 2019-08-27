d73ef1fe7a8003f462a54f8eae7752fbb4d006e1
Pim Kunis <pim@kunis.nl>
2019-08-27 15:01:37 +0200

    Dynamic posts depending on project or post.

--- lib/personal_website_web/views/layout_view.ex
+++ lib/personal_website_web/views/layout_view.ex
@@ -1,6 +1,6 @@
 defmodule PersonalWebsiteWeb.LayoutView do
   use PersonalWebsiteWeb, :view
-  alias PersonalWebsiteWeb.Endpoint
+  alias PersonalWebsiteWeb.Router.Helpers, as: Routes
 
   def get_title(title) do
     title <> " | " <> get_title()
@@ -8,5 +8,22 @@ defmodule PersonalWebsiteWeb.LayoutView do
 
   def get_title do
     "Pim's Thoughts"
+  end
+
+  def post_path(conn, method, post \\ nil)
+  def post_path(%Plug.Conn{path_info: path} = conn, method, post) when hd(path) == "projects" do
+    if post do
+      Routes.project_path(conn, method, post)
+    else
+      Routes.project_path(conn, method)
+    end
+  end
+
+  def post_path(conn, method, post) do
+    if post do
+      Routes.post_path(conn, method, post)
+    else
+      Routes.post_path(conn, method)
+    end
   end
 end
