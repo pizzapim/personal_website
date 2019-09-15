cf09a529a9634350edffb7c9993128b2e786f9c9
Pim Kunis <pim@kunis.nl>
2019-09-15 22:44:22 +0200

    Enable gzip for static files.

--- lib/personal_website_web/endpoint.ex
+++ lib/personal_website_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule PersonalWebsiteWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :personal_website,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
